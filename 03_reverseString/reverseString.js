615ffd5ffca06db26eb5bdb3708f4f053804db30
MrDomoArigato98 <domdob98@gmail.com>
2025-01-20 19:10:44 +0000

    Create for loop over string going backwards

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(normalString) {
 
+    let stringLength = normalString.length;
+    let reversedString;
+    for(let index = stringLength-1; index> 0; index--){
+        reversedString += normalString[index];
+    }
+    return reversedString
 };
 
 // Do not edit below this line
