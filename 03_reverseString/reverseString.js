ae2a2cd9612be1a5081e706e229848e830f388db
MrDomoArigato98 <domdob98@gmail.com>
2025-01-20 19:14:46 +0000

    Reverses string with a for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,15 @@
 const reverseString = function(normalString) {
 
     let stringLength = normalString.length;
-    let reversedString;
-    for(let index = stringLength-1; index> 0; index--){
+    let reversedString ="";
+    for(let index = stringLength-1; index>= 0; index--){
         reversedString += normalString[index];
     }
     return reversedString
+
+
+    // To be fair this also works
+    // return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
