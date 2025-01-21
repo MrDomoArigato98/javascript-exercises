b95ad49b1a1abb2668aeb58968be22e17b81c035
MrDomoArigato98 <domdob98@gmail.com>
2025-01-21 10:38:32 +0000

    Gets 1st decimal place of temp for C and F

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // x °F ≘ (x − 32) × ⁠5/9⁠ °C 
+
+  let celsius = (fahrenheit-32)* (5/9)
+  let roundedCelsius = Math.round(celsius * 10) / 10
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // x °C ≘ (x × ⁠9/5⁠ + 32) °F 
+  let fahrenheit = (celsius * 9/5) + 32
+  let roundedFahrenheit = Math.round(fahrenheit*10) / 10
+  return roundedFahrenheit;
+
 };
 
 // Do not edit below this line
