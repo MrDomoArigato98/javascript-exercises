f9e45e6b15d95de0c122fadea7f83f442b4935d8
MrDomoArigato98 <domdob98@gmail.com>
2025-01-21 10:06:15 +0000

    Returns sum of integers and checks min max

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(rangeIntOne, rangeIntTwo) {
+ 
 
-};
+    let startPosition = Math.min(rangeIntOne, rangeIntTwo);
+    let endPosition = Math.max(rangeIntOne, rangeIntTwo);
+    let sum = 0;
+
+    for (let i = startPosition; i <= endPosition; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
