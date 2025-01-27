99632f4d1c07b144986e0547c5cae0c744fd3b8c
MrDomoArigato98 <domdob98@gmail.com>
2025-01-27 09:52:58 +0000

    Uses map over books object to retrieve titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = books.map(item => item.title)
+    return titles;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
