6243ec2791f66677499a1cd25f500e51a11302f4
MrDomoArigato98 <domdob98@gmail.com>
2025-01-27 09:50:49 +0000

    Fibonacci done using iterative approach

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if(num <0){
+        return "OOPS"
+    }
+    Number(num)
+
+    let fibonacciArray = [0,1];
+
+    for (let index = 0; index < num; index++) {
+        fibonacciArray.push(fibonacciArray[index]+fibonacciArray[index+1])
+    }
+
+    console.log(fibonacciArray)
+
+    return fibonacciArray[num]
 };
 
+fibonacci(4)
 // Do not edit below this line
 module.exports = fibonacci;
