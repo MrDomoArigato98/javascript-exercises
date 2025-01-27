893feb74e1833d57b4101641b117039f7bc177e2
MrDomoArigato98 <domdob98@gmail.com>
2025-01-27 10:12:12 +0000

    Finds oldest using map()
    This one could also just use the reduce() function, which is probably better but it gets the job done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    const getAge = function (birth, death) {
+        if (!death) {
+          death = new Date().getFullYear();
+        }
+        return death - birth;
+      };
 
+    let maxAge = 0;
+    let oldest;
+    people.map(function(person){
+        let personAge = getAge(person.yearOfBirth,person.yearOfDeath)
+        if(personAge > maxAge){
+            oldest = person;
+            maxAge = personAge
+            console.log(oldest)
+        }
+    })
+    console.log(oldest)
+    return oldest;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
